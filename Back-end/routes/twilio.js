e8bf88d6799b5a307a4f5c2e11bff07856400cdd
junaid <junaidali1507@gmail.com>
2021-08-20 13:42:44 +0530

    completed eslint and prettier

--- Back-end/routes/twilio.js
+++
@@ -1,8 +0,0 @@
-// const express = require('express');
-// const router = express.Router();
-// const twilio = require("../controllers/twilio");
-// const middleware = require("../middleware/authorization");
-
-// router.get('/phone-otp' , middleware.verify , twilio.phoneOtpSend);     
-
-// module.exports = router;
