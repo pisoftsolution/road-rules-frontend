0d822a9f46801aea453d9309ce309aead019f766
javeed <jshaik49@gmail.com>
2021-08-20 09:53:32 +0530

    .removed backend

--- Back-end/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const client = require('../controllers/user');
-const middleware = require('../middleware/authorization') 
-
-router.post('/signup' , client.registerUser);
-router.get('/login' , client.loginUser);
-
-module.exports = router;
