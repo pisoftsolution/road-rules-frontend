bc13e017ce34b4727351469545c9a9ff034da1d5
javeed <jshaik49@gmail.com>
2021-08-23 11:45:34 +0530

    Added

--- src/stripeComponent/Stripe.js
+++ src/stripeComponent/Stripe.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import "./stripe.css"
 
 function Stripe(){
     const history = useHistory();
@@ -11,26 +12,61 @@ function Stripe(){
   }; 
     return (
         <div>
-            <button 
-                type="submit"
-                className="btn"
-            >
-                payment of 100$ 
-            </button>
-            <button 
-             onClick={handleRoute1}
-                type="submit"
-                className="btn"
-            >
-                success
-            </button>
-            <button 
-            onClick={handleRoute2}
-                type="submit"
-                className="btn"
-            >
-                Cancel
-            </button>
+            <div className="containerc"> 
+            <div className="row">
+            <div className="card">
+              <div className="card-body">
+                     <form className="" action="/paynow" method="post">
+                        <div className="form-group">
+                            <label >Name: </label>
+                            <input className="form-control" type="text" />
+                        </div>
+                        <div className="form-group">
+                            <label >Email: </label>
+                            <input className="form-control" type="text" />
+                        </div>
+                        <div className="form-group">
+                            <label >Phone: </label>
+                            <input className="form-control" type="text" />
+                        </div>
+                        <div className="form-group">
+                            <label >Amount: </label>
+                            <input className="form-control" type="text" name="amount" value=""/>
+                        </div>
+                        <div className="form-group">
+                            <button className="btn form-control btn-primary">Pay Now</button>
+                        </div>
+                     </form>
+             </div>
+        </div>
+            <div col-md-4>
+                <button 
+                    type="submit"
+                    className="btn"
+                >
+                    payment of 100$ 
+                </button>
+            </div>
+            <div col-md-4>
+                <button 
+                onClick={handleRoute1}
+                    type="submit"
+                    className="btn"
+                >
+                    success
+                </button>
+            </div>
+            <div col-md-4>
+                <button 
+                onClick={handleRoute2}
+                    type="submit"
+                    className="btn"
+                >
+                    Cancel
+                </button>
+            </div>         
+        </div>
+        </div>
         </div>
     )
 }
