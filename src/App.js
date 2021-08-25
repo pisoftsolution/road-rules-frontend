efea91ab128dbe77cc6349c906478dc91598beff
Karan Bains <karanbains.bains@gmail.com>
2021-08-25 13:35:54 +0530

    resolved merge conflict error

--- src/App.js
+++ src/App.js
@@ -6,11 +6,7 @@ import history from './History';
 import Signup from './components/client/auth/signup/Signup';
 import PhoneOtp from './components/client/auth/phoneotp/PhoneOtp';
 import EmailOtp from './components/client/auth/emailotp/EmailOtp'; 
-import Home from './components/client/auth/home/Home';
-import Success from './components/client/Payment/success/success';
-import Cancel from './components/client/Payment/Cancel/cancel';
-import Stripe from './components/client/Payment/payment/stripe';
-import Payment from './components/client/Payment/payment/payment';
+import Home from './components/home/Home';
 
 function App() { 
   return (
@@ -23,16 +19,10 @@ function App() {
         <Route exact path="/phoneotp" component={PhoneOtp} ></Route>
         <Route exact path="/signin" component={Signin} ></Route>
         <Route exact path="/email-otp" component={EmailOtp} ></Route> 
-        <Route exact path="/success" component={Success} ></Route> 
-        <Route exact path="/cancel" component={Cancel} ></Route> 
-        <Route exact path="/stripe" component={Stripe} ></Route>
-        <Route exact path="/payment" component={Payment} ></Route> 
         </Switch>
       </Router> 
     </div>
   );
 }
-
-
 
 export default App;
