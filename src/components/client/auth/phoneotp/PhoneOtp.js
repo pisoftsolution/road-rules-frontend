4c0540014d3a191f989aef6cc46ea9ce3044791f
jshaik49 <jshaik49@gmail.com>
2021-08-26 16:28:08 +0530

    added

--- src/components/client/auth/phoneotp/PhoneOtp.js
+++ src/components/client/auth/phoneotp/PhoneOtp.js
@@ -63,48 +63,6 @@ function PhoneOtp() {
         </div>
       </div>
     </div>
-    // <div className="container-fluid p-0 mt-5.5rem" id="body">
-    //   <div className="container mm">
-    //     <div className="row">
-    //       <div className="col-md-3"></div>
-    //       <div className="col-md-6 ">
-    //         <div className="card box-shadow-lg bg-white" id="card">
-    //           <div>
-    //             <h4 className="mb-4 mt-5" id="signin">
-    //               Please enter the phone OTP
-    //             </h4>
-    //             <div className="m">
-    //               <form onSubmit={handleSubmit}>
-    //                 <label id="email">OTP</label>
-    //                 <div className="input-group ">
-    //                   <input
-    //                     name="phoneOtp"
-    //                     type="text"
-    //                     placeholder="Enter the 6 digit OTP"
-    //                     className="form-control "
-    //                     aria-label="Username"
-    //                     aria-describedby="addon-wrapping"
-    //                     value={formData.phoneOtp}
-    //                     onChange={(e) => {
-    //                       setFormData({
-    //                         ...formData,
-    //                         [e.target.name]: e.target.value
-    //                       });
-    //                     }}
-    //                   />
-    //                 </div>
-    //                 <button onClick={handleRoute} type="submit" className="btn">
-    //                   Submit
-    //                 </button>
-    //               </form>
-    //             </div>
-    //           </div>
-    //         </div>
-    //         <div className="col-md-2"></div>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
   );
 }
 
