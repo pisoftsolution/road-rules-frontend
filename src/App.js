4ba0a9f0bcfcd8b697cd05fbd130d9322190fb05
junaid <junaidali1507@gmail.com>
2021-09-02 00:39:46 +0530

    added files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import './App.css';
 import Signin from './components/client/auth/login/Signin';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import history from './History';
+import { Route, Switch } from 'react-router-dom';
 import Signup from './components/client/auth/signup/Signup';
 import PhoneOtp from './components/client/auth/phoneotp/PhoneOtp';
 import EmailOtp from './components/client/auth/emailotp/EmailOtp';
@@ -25,7 +24,7 @@ import Slots from './components/admin/home/Slots';
 import AllRides from './components/admin/home/AllRides';
 import Logout from './components/admin/home/Logout';
 
-const useStyles = makeStyles({ 
+const useStyles = makeStyles({
   container: {
     display: "flex"
   }
@@ -36,33 +35,31 @@ function App() {
   return (
     <div className="App">
       {/* <Navbar /> */}
-      <Router history={history}>
-        <Switch>
-          <Route exact path="/" component={Signin}></Route>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/signup" component={Signup}></Route>
-          <Route exact path="/email-otp" component={EmailOtp}></Route>
-          <Route exact path="/phoneotp" component={PhoneOtp}></Route>
-          <Route exact path="/signin" component={Signin}></Route>
-          <Route exact path="/success" component={Success}></Route>
-          <Route exact path="/cancel" component={Cancel}></Route>
-          <Route exact path="/stripe" component={Stripe}></Route>
-          <Route exact path="/payment" component={Payment}></Route>
-          {/* <Route exact path="/admin" component={Sidebar}></Route> */}
-         <div className={classes.container}>
-           <Sidebar />
-         <Route exact path="/about" render={props => <About {...props} />} />
-        <Route exact path="/aboutcards" render={props => <AboutCards {...props} />} />
-        <Route exact path="/testimonial" render={props => <Testimonials {...props} />} />
-        <Route exact path="/choice" render={props => <Choice {...props} />} />
-        <Route exact path="/user" render={props => <User {...props} />} />
-        <Route exact path="/instructor" render={props => <Instructor {...props} />} />
-        <Route exact path="/slots" render={props => <Slots {...props} />} />
-        <Route exact path="/allrides" render={props => <AllRides {...props} />} />
-        <Route exact path="/logout" render={props => <Logout {...props} />} />
-         </div>
-        </Switch>
-      </Router>
+      <Switch>
+        <Route exact path="/" component={Signin}></Route>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/signup" component={Signup}></Route>
+        <Route exact path="/email-otp" component={EmailOtp}></Route>
+        <Route exact path="/phoneotp" component={PhoneOtp}></Route>
+        <Route exact path="/signin" component={Signin}></Route>
+        <Route exact path="/success" component={Success}></Route>
+        <Route exact path="/cancel" component={Cancel}></Route>
+        <Route exact path="/stripe" component={Stripe}></Route>
+        <Route exact path="/payment" component={Payment}></Route>
+        {/* <Route exact path="/admin" component={Sidebar}></Route> */}
+        <div className={classes.container}>
+          <Sidebar />
+          <Route exact path="/about" render={props => <About {...props} />} />
+          <Route exact path="/aboutcards" render={props => <AboutCards {...props} />} />
+          <Route exact path="/testimonial" render={props => <Testimonials {...props} />} />
+          <Route exact path="/choice" render={props => <Choice {...props} />} />
+          <Route exact path="/user" render={props => <User {...props} />} />
+          <Route exact path="/instructor" render={props => <Instructor {...props} />} />
+          <Route exact path="/slots" render={props => <Slots {...props} />} />
+          <Route exact path="/allrides" render={props => <AllRides {...props} />} />
+          <Route exact path="/logout" render={props => <Logout {...props} />} />
+        </div>
+      </Switch>
     </div>
   );
 }
