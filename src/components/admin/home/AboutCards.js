6676016bfcdd9430e23d4be88ba5085553f57608
junaid <junaidali1507@gmail.com>
2021-09-06 10:30:37 +0530

    connected front-end to back-end

--- src/components/admin/home/AboutCards.js
+++ src/components/admin/home/AboutCards.js
@@ -1,36 +1,98 @@
-import React, { useState } from "react";
-import { Button } from "react-bootstrap";
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux'
+import { Table, Button } from 'react-bootstrap'
 import Modal from 'react-bootstrap/Modal'
 import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
 import './All.css'
+import { addAboutcards, editAboutcards, getAboutcards } from '../../../redux/actions/aboutcards';
+
 function AboutCards() {
+
+  const initialState = { subHeading: "", text: "" };
+  const [formData, setFormData] = useState(initialState);
+  const [editId, setEditId] = useState("");
+  const [shouldCall, setShouldCall] = useState(false);
+  const dispatch = useDispatch();
+  const aboutcard = useSelector(state => state.aboutcardsReducer?.aboutcardsData?.b);
+
+  const editHandler = () => {
+    dispatch(editAboutcards());
+  }
+  useEffect(() => {
+    dispatch(getAboutcards())
+  }, [shouldCall])
+
+  const handleEditSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+    dispatch(editAboutcards(formData, editId))
+      .then(res => {
+        console.log(res);
+        setShouldCall(!shouldCall);
+        setShow(false);
+        setFormData(initialState);
+      })
+  }
+
+  const handleAddSubmit = (e) => {
+    e.preventDefault();
+    // console.log(formData);
+    dispatch(addAboutcards(formData, aboutcard))
+      .then(res => {
+        console.log(res);
+        setShouldCall(!shouldCall);
+      })
+  }
+
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
-  const handleShow = () => {
+  const handleShow = (id, subHeading, text) => {
+    setEditId(id)
+    setFormData({
+      subHeading: subHeading,
+      text: text
+    })
     setShow(true);
   }
+
+  const [shows, setShows] = useState(false);
+  const handleCloses = () => setShows(false);
+  const handleShows = (subHeading, text) => {
+    setFormData({
+      subHeading: subHeading,
+      text: text
+    })
+    setShows(true);
+  }
   return (
-    <div className="tab">
-      <h4>About Cards</h4>
+    <div className="Apps">
+      <h1 className="headline">About cards</h1>
       <Modal show={show} onHide={handleClose}  >
         <Modal.Header>
-          <Modal.Title>Edit a Slot</Modal.Title>
+          <Modal.Title>Edit Slot</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <form>
+          <form onSubmit={handleEditSubmit}>
             <div className="form-group">
-              <label>Heading</label>
+              <label>subHeading</label>
               <InputGroup className="mb-3">
                 <FormControl
-                  name="heading"
+                  name="subHeading"
                   type="text"
-                  placeholder="Enter Heading"
+                  placeholder="Enter subHeading"
                   aria-label="Username"
                   aria-describedby="basic-addon1"
+                  value={formData.subHeading}
+                  onChange={(e) => {
+                    setFormData({
+                      ...formData,
+                      [e.target.name]: e.target.value,
+                    })
+                  }}
+                  required
                 />
               </InputGroup>
-
               <label>Text</label>
               <InputGroup className="mb-3">
                 <FormControl
@@ -39,6 +101,14 @@ function AboutCards() {
                   type="text"
                   aria-label="Username"
                   aria-describedby="basic-addon1"
+                  value={formData.text}
+                  onChange={(e) => {
+                    setFormData({
+                      ...formData,
+                      [e.target.name]: e.target.value,
+                    })
+                  }}
+                  required
                 />
               </InputGroup>
               <Button
@@ -57,33 +127,100 @@ function AboutCards() {
           </form>
         </Modal.Body>
       </Modal>
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th scope="col">S.No</th>
-            <th scope="col">Name</th>
-            <th scope="col">Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td><Button onClick={handleShow}>Edit</Button></td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td><Button onClick={handleShow}>Edit</Button></td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>Jacob</td>
-            <td><Button onClick={handleShow}>Edit</Button></td>
-          </tr>
-        </tbody>
-      </table>
+      <div>
+        <Modal show={shows} onHide={handleCloses}  >
+          <Modal.Header>
+            <Modal.Title>Add About-Cards</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <form onSubmit={handleAddSubmit}>
+              <div className="form-group">
+                <label>subHeading</label>
+                <InputGroup className="mb-3">
+                  <FormControl
+                    name="subHeading"
+                    placeholder="Enter subHeading"
+                    type="text"
+                    className=""
+                    onChange={(e) => {
+                      setFormData({
+                        ...formData,
+                        [e.target.name]: e.target.value,
+                      })
+                    }}
+                    required
+                  />
+                </InputGroup>
+
+                <label>Text</label>
+                <InputGroup className="mb-3">
+                  <FormControl
+                    name="text"
+                    placeholder="Enter Text Here"
+                    onChange={(e) => {
+                      setFormData({
+                        ...formData,
+                        [e.target.name]: e.target.value,
+                      })
+                    }}
+                    required
+                  />
+                </InputGroup>
+                <Button
+                  className="sub"
+                  type="submit"
+                  onClick={handleCloses}
+                >
+                  Update
+                </Button>
+                <Button
+                  className="cancel"
+                  onClick={handleCloses}
+                >
+                  Close
+                </Button>
+              </div>
+            </form>
+          </Modal.Body>
+        </Modal>
+        <Button
+          className="add"
+          onClick={() => handleShows()}
+        >
+          Add AboutCards
+        </Button>
+      </div>
+      <Table striped bordered hover>
+        <tr>
+          <th>subHeading</th>
+          <th >Text</th>
+          <th >Actions </th>
+        </tr>
+        {aboutcard && aboutcard.length > 0 ?
+          aboutcard.map(b => {
+            return (
+              <>
+                <tr key={b._id}>
+                  <td>{b.subHeading}</td>
+                  <td>{b.text}</td>
+                  <Button
+                    onClick={() => {
+                      editHandler(b._id)
+                      handleShow(b._id, b.subHeading, b.text)
+                    }}
+                    className="btn1"
+
+                  >
+                    Edit
+                  </Button>
+                </tr>
+
+              </>
+            )
+          }) : ''}
+      </Table>
     </div>
   )
 }
-export default AboutCards
+
+export default AboutCards;
---
+++ src/redux/reducers/aboutcards.js
@@ -0,0 +1,29 @@
+import { ADD_ABOUTCARDS, EDIT_ABOUTCARDS, GET_ABOUTCARDS } from "../constants";
+
+export default (state = { aboutcardsData: null }, action) => {
+
+    switch (action.type) {
+        case GET_ABOUTCARDS:
+            console.log(action?.data);
+            return {
+                ...state,
+                aboutcardsData: action?.data
+            }
+        case ADD_ABOUTCARDS:
+            console.log(action?.data);
+            return {
+                ...state,
+                aboutcardsData: action?.data
+            }
+        case EDIT_ABOUTCARDS:
+            console.log(action?.data);
+            return {
+                ...state,
+                aboutcardsData: action?.data
+            }
+        default:
+            return {
+                ...state
+            }
+    }
+};
